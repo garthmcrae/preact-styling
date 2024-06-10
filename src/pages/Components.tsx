fc7d82ed5996c29caf57f9f43b7b0c71405856a0
Garth McRae <garthmcrae@gmail.com>
2024-06-10 14:38:43 +1000

    Labels.

--- src/pages/Components.tsx
+++ src/pages/Components.tsx
@@ -138,6 +138,7 @@ export function Components() {
           <Code>{pictureRaw}</Code>
         </Box>
         <Box>
+          <Label>1+ MB image</Label>
           <Picture
             url={
               "https://images.unsplash.com/photo-1462331940025-496dfbfc7564?q=80&w=3011&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
